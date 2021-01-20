eeca49b60d078f4b12fad893a8afcefcc4ed70aa
Surya Thakur <77577564+Surya0705@users.noreply.github.com>
2021-01-20 23:15:48 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,6 +1,6 @@
 import wolframalpha # Importing the Wolfram|Alpha library for Project.
 
-AppID = "LRWVAL-77HYY7K9U6" # This is the AppID I got from Wolfram|Alpha for integration in my Project.
+AppID = "Your AppID" # Get the AppID from Wolfram|Alpha and put it into your Project.
 Client = wolframalpha.Client(AppID) # This gives the program my AppID telling it I am a Client.
 
 while(1): # I used this to put my Program in while loop so that it doesn't stop just after giving a single output.
